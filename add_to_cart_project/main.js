84757a3fa72d2fd88527dc9d107222416873bd6e
saiful Islam <saifulmeraz897@gmail.com>
2021-12-21 23:14:44 +0600

    More practice vue

--- add_to_cart_project/main.js
+++ add_to_cart_project/main.js
@@ -3,12 +3,11 @@ const app =Vue.createApp({
         return{
             productName:"Socks",
             cart:0,
-            product:'img/socks_green.jpg',
-            inventry:10,
+            selectedVariant: 0,
             product_deatils:['100% cotton','10% spandix'],
             product_id_color:[
-                {id:123,color:"green",image:"img/socks_green.jpg"},
-                {id:124,color:"blue",image:"img/socks_blue.jpg"}
+                {id:123,color:"green",image:"img/socks_green.jpg",quantity:50},
+                {id:124,color:"blue",image:"img/socks_blue.jpg",quantity:0}
             ]
         }
     },
@@ -19,9 +18,17 @@ const app =Vue.createApp({
         reduceToCart(){
             this.cart -=1
     },
-        updateImage(variantImage){
-            this.product=variantImage
-        }
+        updateVariaint(index){
+            this.selectedVariant =index
+    }
     },
+    computed:{
+        product(){
+            return this.product_id_color[this.selectedVariant].image
+        },
+        inStock(){
+            return this.product_id_color[this.selectedVariant].quantity
+        }
+    }
 })
 app.mount('#app')
