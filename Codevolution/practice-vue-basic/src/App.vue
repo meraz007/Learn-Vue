a29d35cfac3b85c8057b50d1d033e0ac0468574b
saiful Islam <saifulmeraz897@gmail.com>
2021-12-23 17:23:56 +0600

    adding new data

---
+++ Codevolution/practice-vue-basic/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ <h1>Hello {{ name }}</h1>
+ <p v-text="age"></p>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      name :'saiful',
+      age :'He is ' + 25 +' years old'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
