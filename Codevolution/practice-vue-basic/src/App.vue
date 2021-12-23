0e2d49e234677d1d62f350f6d56344bdb50b1977
saiful Islam <saifulmeraz897@gmail.com>
2021-12-23 22:05:07 +0600

    class binding

--- Codevolution/practice-vue-basic/src/App.vue
+++ Codevolution/practice-vue-basic/src/App.vue
@@ -2,6 +2,10 @@
  <h1>Hello {{ name }}</h1>
  <p v-text="age"></p>
  <p v-html="details"></p>
+ <button :disabled="!isDisabled">Bind</button>
+ <p :class="[isPromoted ? 'color':'new']" >this is a paragraph</p>
+ <button >Click</button>
+ 
 </template>
 
 <script>
@@ -12,7 +16,10 @@ export default {
     return{
       name :'saiful',
       age :'He is ' + 25 +' years old',
-      details : '<b>My life is a fucking Awesome!</b>'
+      details : '<b>My life is a fucking Awesome!</b>',
+      headingId : 'Heading',
+      isDisabled : true,
+      isPromoted :true
     }
   }
 }
@@ -27,4 +34,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.color{
+  background-color: black;
+  color: white;
+  width: 200px;
+  text-align: center;
+  margin: auto;
+  padding: 10px;
+  }
+  .new{
+    background-color: tomato;
+  }
 </style>
