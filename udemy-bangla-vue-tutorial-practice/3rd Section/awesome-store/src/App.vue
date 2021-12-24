4359d0dac8d825e5f3b789c0c2f69bc454e21387
saiful Islam <saifulmeraz897@gmail.com>
2021-12-25 01:20:23 +0600

    create basic layout

--- udemy-bangla-vue-tutorial-practice/3rd Section/awesome-store/src/App.vue
+++ udemy-bangla-vue-tutorial-practice/3rd Section/awesome-store/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-   <div>
   <b-navbar toggleable="lg" type="dark" variant="dark">
     <div class="container">
       <b-navbar-brand href="#">NavBar</b-navbar-brand>
@@ -11,12 +10,103 @@
         <button class="btn btn-outline-light" type="submit">Search</button>
         </div>
       </div>
-</form>
+      </form>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     </div>
-     </b-navbar>
-</div>
+  </b-navbar>
+  <!--Card Section-->
+  <b-container class="bv-example-row mt-4">
+  <b-row>
+    <b-col cols="9">
+      <b-row>
+        <b-col cols="3">
+          <div>
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
 
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+          </div>
+        </b-col>
+        <b-col cols="3">
+           <div>
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+          </div>
+        </b-col>
+        <b-col cols="3">
+           <div>
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+          </div>
+        </b-col>
+        <b-col cols="3">
+           <div>
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+          </div>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col cols="3">
+      <b-list-group>
+        <b-list-group-item>Item - Price </b-list-group-item>
+        <b-list-group-item>Item - Price </b-list-group-item>
+        <b-list-group-item>Item - Price </b-list-group-item>
+        
+      </b-list-group>
+    </b-col>
+  </b-row>
+</b-container>
   </div>
 </template>
 
