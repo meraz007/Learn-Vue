d9b706cefca48ad4cb961c6918aef57d77a5ff1a
saiful Islam <saifulmeraz897@gmail.com>
2022-01-31 20:51:20 +0600

    adding slot

---
+++ slots/src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class="card">
+      <slot></slot>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- slots/src/views/Home.vue
+++ slots/src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="home">
-   <h1>Home</h1>
+    <div class="row">
+      <div class="col-md-6">
+        <Card>
+          <div class="card-header">
+            Featured
+          </div>
+        </Card>
+        <Card>
+          <div class="card-header">
+            Food
+          </div>
+        </Card>
+      </div>
+      <div class="col-md-6">
+        <Card>
+          <div class="card-header">
+            Sports
+          </div>
+        </Card>
+        <Card>
+          <div class="card-header">
+            Movies
+          </div>
+        </Card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Card from '../components/Card.vue'
 export default {
   name: 'Home',
   components: {
-    
+    Card
   }
 }
 </script>
